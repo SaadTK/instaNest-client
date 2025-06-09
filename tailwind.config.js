6b4114283f6fc0fa9e4001ad0d72a9d9da543c24
SaadTK <tahmid.karim.saad@gmail.com>
2025-06-09 10:12:32 +0600

    I didn't most of the frontend code and I closed the project and I have to do the frontend all over again

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#B8860B",
+        secondary: "#8B4513",
+        accent: "#D2B48C",
+        background: "#F5F1EA",
+        textPrimary: "#3E2F1C",
+      },
+      fontFamily: {
+        heading: ["Montserrat", "sans-serif"],
+        body: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
